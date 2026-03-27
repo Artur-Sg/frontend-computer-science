47bc9e9bc287917d0230005291aa2241cb02c383
Artur <aasgibnev@gmail.com>
2026-03-28 02:26:00 +0400

    refactor

--- src/modules/solutions/bcd/bcd.test.ts
+++ src/modules/solutions/bcd/bcd.test.ts
@@ -1,7 +1,7 @@
 import assert from 'node:assert/strict';
 import { BCD8421 } from './bcd';
 
-function expectEqual(actual: unknown, expected: unknown, msg: string) {
+function expectEqual(actual: unknown, expected: unknown, msg: string): void {
   assert.equal(actual, expected, msg);
 }
 
@@ -30,6 +30,7 @@ assert.throws(() => n.at(-6), RangeError, 'at(-6) должен бросать Ra
 
 // === Ноль ===
 const z = new BCD8421(0);
+
 expectEqual(z.toString(), '0', 'toString(0) -> "0"');
 expectEqual(z.toBigint(), 0n, 'toBigint(0) -> 0n');
 expectEqual(z.toNumber(), 0, 'toNumber(0) -> 0');
@@ -40,6 +41,7 @@ assert.throws(() => z.at(-2), RangeError, 'at(-2) для 0 вне диапазо
 
 // === Одна цифра ===
 const single = new BCD8421(7);
+
 expectEqual(single.toString(), '7', 'одна цифра toString');
 expectEqual(single.toBigint(), 7n, 'одна цифра toBigint');
 expectEqual(single.toNumber(), 7, 'одна цифра toNumber');
@@ -50,6 +52,7 @@ assert.throws(() => single.at(-2), RangeError);
 
 // === Нечётная длина ===
 const odd = new BCD8421(12345);
+
 expectEqual(odd.toString(), '12345', 'нечётная длина корректно восстанавливается');
 expectEqual(odd.toBigint(), 12345n, 'нечётная длина toBigint');
 expectEqual(odd.toNumber(), 12345, 'нечётная длина toNumber');
@@ -68,6 +71,7 @@ expectEqual(odd.at(-5), 1, 'odd at(-5)');
 
 // === Чётная длина ===
 const even = new BCD8421(1234);
+
 expectEqual(even.toString(), '1234', 'чётная длина');
 expectEqual(even.at(0), 4, 'even at(0)');
 expectEqual(even.at(1), 3, 'even at(1)');
@@ -81,11 +85,13 @@ expectEqual(even.at(-4), 1, 'even at(-4)');
 
 // === Большое число (bigint) ===
 const big = new BCD8421(1234567890n);
+
 expectEqual(big.toString(), '1234567890', 'toString() для bigint');
 expectEqual(big.toBigint(), 1234567890n, 'toBigint() для bigint');
 
 // === Число с нулями внутри ===
 const middleZeros = new BCD8421(9005);
+
 expectEqual(middleZeros.toString(), '9005', 'число с нулями внутри');
 expectEqual(middleZeros.at(0), 5, '9005 at(0)');
 expectEqual(middleZeros.at(1), 0, '9005 at(1)');
